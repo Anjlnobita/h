9ee0bed2551f9c9434e59d7e4e85af969cc04f3c
Anjlnobita <jiteshkumar7200@gmail.com>
2025-01-28 05:00:27 +0530

    Update mongo.py

--- RishuMusic/core/mongo.py
+++ RishuMusic/core/mongo.py
@@ -12,3 +12,30 @@ try:
 except:
     LOGGER(__name__).error("Failed to connect to your Mongo Database.")
     exit()
+
+
+
+
+import asyncio
+import sys
+
+from motor import motor_asyncio
+from config import MONGO_DB_URI 
+from pymongo import MongoClient
+from pymongo.errors import ServerSelectionTimeoutError
+
+
+MONGO_PORT = 27017
+MONGO_DB_URI = MONGO_DB_URI
+MONGO_DB = "Anon"
+
+
+client = MongoClient()
+client = MongoClient(MONGO_DB_URI, MONGO_PORT)[MONGO_DB]
+motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI, MONGO_PORT)
+mongodb = motor[MONGO_DB]
+mongodb = client["Anon"]
+try:
+    asyncio.get_event_loop().run_until_complete(motor.server_info())
+except ServerSelectionTimeoutError:
+    sys.exit(log.critical("Can't connect to mongodb! Exiting..."))
