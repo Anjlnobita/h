d1b99dd8663890553d035275cac67c895b471a7f
Anjlnobita <jiteshkumar7200@gmail.com>
2025-01-28 05:13:45 +0530

    Update __init__.py

--- RishuMusic/__init__.py
+++ RishuMusic/__init__.py
@@ -2,13 +2,14 @@ from RishuMusic.core.bot import SACHIN
 from RishuMusic.core.dir import dirr
 from RishuMusic.core.git import git
 from RishuMusic.core.userbot import Userbot
-from RishuMusic.misc import  heroku
+from RishuMusic.misc import dbb, heroku
 
 from SafoneAPI import SafoneAPI
 from .logging import LOGGER
 
 dirr()
 git()
+dbb()
 heroku()
 
 app = SACHIN()
